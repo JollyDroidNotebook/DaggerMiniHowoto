4ca20ad0f2730ed624f6d9903dab102b0622143a
Igor Tseglevskiy <igor.tseglevskiy@gmail.com>
2017-12-01 12:01:17 +0300

    zero app

---
+++ app/src/main/java/ru/jollydroid/daggerminihowto/MainActivity.kt
@@ -0,0 +1,66 @@
+/*
+    Данная лицензия разрешает лицам, получившим копию программы и сопутствующей
+    документации (в дальнейшем именуемыми «Программное Обеспечение»),
+    безвозмездно использовать Программное Обеспечение без ограничений, включая
+    неограниченное право на использование, копирование, изменение, добавление,
+    публикацию, распространение, сублицензирование и/или продажу копий
+    Программного Обеспечения, также как и лицам, которым предоставляется данное
+    Программное Обеспечение, при соблюдении следующих условий:
+
+    Указанное выше уведомление об авторском праве и данные условия должны быть
+    включены во все копии или значимые части данного Программного Обеспечения.
+
+    ДАННОЕ ПРОГРАММНОЕ ОБЕСПЕЧЕНИЕ ПРЕДОСТАВЛЯЕТСЯ «КАК ЕСТЬ», БЕЗ КАКИХ-ЛИБО
+    ГАРАНТИЙ, ЯВНО ВЫРАЖЕННЫХ ИЛИ ПОДРАЗУМЕВАЕМЫХ, ВКЛЮЧАЯ, НО НЕ
+    ОГРАНИЧИВАЯСЬ ГАРАНТИЯМИ ТОВАРНОЙ ПРИГОДНОСТИ, СООТВЕТСТВИЯ ПО ЕГО
+    КОНКРЕТНОМУ НАЗНАЧЕНИЮ И ОТСУТСТВИЯ НАРУШЕНИЙ ПРАВ. НИ В КАКОМ СЛУЧАЕ
+    АВТОРЫ ИЛИ ПРАВООБЛАДАТЕЛИ НЕ НЕСУТ ОТВЕТСТВЕННОСТИ ПО ИСКАМ О ВОЗМЕЩЕНИИ
+    УЩЕРБА, УБЫТКОВ ИЛИ ДРУГИХ ТРЕБОВАНИЙ ПО ДЕЙСТВУЮЩИМ КОНТРАКТАМ, ДЕЛИКТАМ
+    ИЛИ ИНОМУ, ВОЗНИКШИМ ИЗ, ИМЕЮЩИМ ПРИЧИНОЙ ИЛИ СВЯЗАННЫМ С ПРОГРАММНЫМ
+    ОБЕСПЕЧЕНИЕМ ИЛИ ИСПОЛЬЗОВАНИЕМ ПРОГРАММНОГО ОБЕСПЕЧЕНИЯ ИЛИ ИНЫМИ
+    ДЕЙСТВИЯМИ С ПРОГРАММНЫМ ОБЕСПЕЧЕНИЕМ.
+
+    Кроме содержимого в этом уведомлении, ни название "Jolly Droid",
+    ни названия "Мобильное Изменение" и "Mobile Dimenstion",
+    ни имена держателей авторских прав не должны быть использованы в
+    рекламе или иным способом, чтобы увеличивать продажу, использование
+    или другие работы в этом Программном обеспечении без предшествующего
+    письменного разрешения.
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+
+    Except as contained in this notice, the name of Jolly Droid or the
+    name of Mobile Dimension or name(s) the copyright holders shall not be
+    used in advertising or otherwise to promote the sale, use or other dealings
+    in this Software without prior written authorization.
+
+ */
+
+package ru.jollydroid.daggerminihowto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
